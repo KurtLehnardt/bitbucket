72151a6c52797f984fd8b5ae4815a89db9f0eb79
Kurt Lehnardt <gutbookvonnegut@gmail.com>
2023-02-21 15:55:35 -0700

    Add code in file types: .ts, .yml Remove code in file types: .ts, .yml (commit 2/2)

---
+++ example_0292.ts
@@ -0,0 +1,12 @@
+// TypeScript contribution
+interface ExampleInterface {
+    name: string;
+    version: number;
+}
+
+const example: ExampleInterface = {
+    name: "Hello World",
+    version: 1
+};
+
+export default example;
